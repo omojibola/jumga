3b932a6ea9a6382dfb719312615c1d4545e03eda
omojibola <omojibolaorija@gmail.com>
2020-12-30 16:48:32 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Start Working ASAP Bitch!!!</p>
+        <p>Jibola testing</p>
         <a
           className="App-link"
           href="https://reactjs.org"
